f9b80c149eeaa1a58520279a6fa63f250f94ee74
Philemon Eichin <phileicht@gmx.de>
2018-06-11 15:31:43 +0200

    Add InfoView
    Added control that displays version information.
    Add Ribbon Resources
    Rename Info ViewModel

---
+++ Core/ViewModels/InfoViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace phirSOFT.Applications.MusicStand.Core.ViewModels
+{
+    public class InfoViewModel
+    {
+        public InfoViewModel()
+        {
+            ApplicationVersion = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                ?.InformationalVersion;
+
+            ApplicationTitle = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyTitleAttribute>()
+                ?.Title;
+
+            CoreVersion = Assembly.GetAssembly(typeof(InfoViewModel)).GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                ?.InformationalVersion;
+
+            // Todo: Load actual contract type
+            ContractVersion =  Assembly.GetAssembly(typeof(InfoViewModel)).GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                ?.InformationalVersion;
+
+        }
+
+        public string ApplicationTitle { get; }
+
+        public string ApplicationVersion { get; }
+
+        public string CoreVersion { get; }
+
+        public string ContractVersion { get; }
+    }
+}
---
+++ Core/Views/InfoView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace phirSOFT.Applications.MusicStand.Core.Views
+{
+    /// <summary>
+    /// Interaction logic for InfoView
+    /// </summary>
+    public partial class InfoView
+    {
+        public InfoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
