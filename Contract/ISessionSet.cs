4ddde5d4cf142a6e50136f81d415762c6f1e1285
Philemon Eichin <phileicht@gmx.de>
2018-06-13 09:00:59 +0200

    Create SessionSet Interface
    Add ISessionSet-type which will hold a session set.
    Add ISessionSetItem-type which is required by this type, but currently a dummy type.

---
+++ Contract/ISessionSet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phirSOFT.Applications.MusicStand.Contract
+{
+    /// <summary>
+    ///     Provides a set of elements (like songs) played during a session.
+    /// </summary>
+    public interface ISessionSet : IList<ISessionSetItem>
+    {
+        /// <summary>
+        /// Returns a flatten view of the session set.
+        /// </summary>
+        /// <returns>A readonly collection that maps to the flatten view.</returns>
+        IReadOnlyList<ISessionSetItem> GetFlatView();
+    }
+}
---
+++ Contract/ISessionSetItem.cs
@@ -0,0 +1,10 @@
+﻿namespace phirSOFT.Applications.MusicStand.Contract
+{
+    /// <summary>
+    /// Represents an item in a <see cref="ISessionSet"/>
+    /// </summary>
+    public interface ISessionSetItem
+    {
+        
+    }
+}
