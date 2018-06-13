cb96d4b4736addd6b42f26ec006d491d5de530e3
Philemon Eichin <phileicht@gmx.de>
2018-06-13 08:40:24 +0200

    Add ApplicationPaths class
    Add paths to the class, which can be reused by modules to store data.
    - AppDataFolder -> %AppData%\phirSOFT\MusicStand
    - LocalAppDataFolder -> %LocalAppData%\phirSOFT\MusicStand

---
+++ Contract/ApplicationPaths.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace phirSOFT.Applications.MusicStand.Contract
+{
+    /// <summary>
+    /// Provides path that the application uses.
+    /// </summary>
+    public static class ApplicationPaths
+    {
+        /// <summary>
+        /// Gets the roaming app data folder of this application.
+        /// </summary>
+        public static readonly string AppDataFolder =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "phirSOFT",
+                "MusicStand");
+
+        /// <summary>
+        /// Gets the local app data folder of this application.
+        /// </summary>
+        public static readonly string LocalAppDataFolder =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "phirSOFT",
+                "MusicStand");
+    }
+}
