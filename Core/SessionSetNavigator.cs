d36a340b28f1d29254033ad6c3fbddec948939f0
Philemon Eichin <phileicht@gmx.de>
2018-06-13 10:54:15 +0200

    Add SessionSetNavigator Stub
    Add a stub for the session set navigator to write unit tests.

---
+++ Core/SessionSetNavigator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using phirSOFT.Applications.MusicStand.Contract;
+
+namespace phirSOFT.Applications.MusicStand.Core
+{
+    internal class SessionSetNavigator : ISessionSetNavigator
+    {
+        public ISessionSet SessionSet { get; set; }
+        public ISessionSetItem Current { get; }
+        public int Index { get; set; }
+        public bool MoveFirst()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MovePrevious()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveLast()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
