ae7460a326c3123bc32655da9044099639a501e0
Philemon Eichin <phileicht@gmx.de>
2018-06-13 09:04:04 +0200

    Add SessionSetGroup interface
    Enable grouping of ISessionSetItems

---
+++ Contract/ISessionSetItemGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phirSOFT.Applications.MusicStand.Contract
+{
+    /// <summary>
+    /// Provies a logical container to organize <see cref="ISessionSetItem"/>s in a hirachial way.
+    /// </summary>
+    public interface ISessionSetItemGroup : ISessionSetItem, IList<ISessionSetItem>
+    {
+    }
+}
