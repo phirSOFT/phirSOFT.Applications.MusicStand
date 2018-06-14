f9ab834f0655947e63ab2889e8b6f3131a50395c
Philemon Eichin <phileicht@gmx.de>
2018-06-14 12:08:08 +0200

    Add MonitorChildren Interface
    Ann an interface that can notify an subscriber, wheather itself can
    monitor all of its children, of if a poll is required to update the
    subscriber.

---
+++ Core/IMonitorChildren.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Specialized;
+
+namespace phirSOFT.Applications.MusicStand.Core
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// A collection implementing this interface notifies the subsriber, wheater it can monitor all of its children.
+    /// </summary>
+    /// <remarks>
+    /// If this interface is not present on a collection, it is assumed that <see cref="P:phirSOFT.Applications.MusicStand.Core.IMonitorChildren.CanMonitorAllChildren" /> is allways <see langword="false" />
+    /// </remarks>
+    public interface IMonitorChildren : INotifyCollectionChanged
+    {
+        /// <summary>
+        /// If true, the collection can monitor all of its children.
+        /// </summary>
+        bool CanMonitorAllChildren { get; }
+
+        /// <summary>
+        /// This event is raised, when <see cref="CanMonitorAllChildren"/> changed.
+        /// </summary>
+        event EventHandler CanMonitorAllChildrenChanged;
+    }
+}
