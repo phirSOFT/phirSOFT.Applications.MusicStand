227f179646aa0b3289214ef7cad472e1e4199c05
Philemon Eichin <phileicht@gmx.de>
2018-06-14 14:25:36 +0200

    Fix various Mock types
    FlattenableMock:
    -	Support empty array inside the mock
    - 	Generate Equality members
    -	Override `ToString` to enhance test output reading.
    
    FlattenableLeaf:
    -	Generate Equality members
    -	Override `ToString` to enhance test output reading.

--- Core.Test/Mocks/FlattenableLeaf.cs
+++ Core.Test/Mocks/FlattenableLeaf.cs
@@ -1,6 +1,9 @@
-﻿namespace phirSOFT.Applications.MusicStand.Core.Test.Mocks
+﻿using System;
+using System.Collections.Generic;
+
+namespace phirSOFT.Applications.MusicStand.Core.Test.Mocks
 {
-    struct FlattenableLeaf<T> : INode<T>
+    struct FlattenableLeaf<T> : INode<T>, IEquatable<FlattenableLeaf<T>>
     {
         public FlattenableLeaf(T value)
         {
@@ -8,5 +11,35 @@
         }
 
         public T Value { get; }
+
+        public bool Equals(FlattenableLeaf<T> other)
+        {
+            return EqualityComparer<T>.Default.Equals(Value, other.Value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            return obj is FlattenableLeaf<T> leaf && Equals(leaf);
+        }
+
+        public override int GetHashCode()
+        {
+            return EqualityComparer<T>.Default.GetHashCode(Value);
+        }
+
+        public static bool operator ==(FlattenableLeaf<T> left, FlattenableLeaf<T> right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(FlattenableLeaf<T> left, FlattenableLeaf<T> right)
+        {
+            return !left.Equals(right);
+        }
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
     }
 }
--- Core.Test/Mocks/FlattenableMock.cs
+++ Core.Test/Mocks/FlattenableMock.cs
@@ -8,36 +8,82 @@ using phirSOFT.Applications.MusicStand.Contract;
 
 namespace phirSOFT.Applications.MusicStand.Core.Test.Mocks
 {
-    struct FlattenableMock<T> : IFlattenable<T>, INode<T>
+    struct FlattenableMock<T> : IFlattenable<INode<T>>, INode<T>, IEquatable<FlattenableMock<T>>
     {
-        private T[] data;
+        private readonly INode<T>[] _data;
 
-        public FlattenableMock(T[] data, T value)
+        public FlattenableMock(INode<T>[] data, T value)
         {
-            this.data = data;
+            this._data = data;
             Value = value;
-            var list = new List<T>() { value };
-            list.AddRange(data);
+            var list = new List<INode<T>>();
             FlatView = list;
+
+   
+            if(data!=null)
+                list.AddRange(data);
 
         }
 
-        public IEnumerator<T> GetEnumerator()
+        public FlattenableMock(T[] data, T value)
+            :this(data.Select(i => (INode<T>) new FlattenableLeaf<T>(i)).ToArray(), value)
         {
-            return ((IList<T>)data).GetEnumerator();
+            
+
+        }
+
+        public IEnumerator<INode<T>> GetEnumerator()
+        {
+            return ((IList<INode<T>>)_data).GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return data.GetEnumerator();
+            return _data.GetEnumerator();
         }
 
-        public int Count => data.Length;
+        public int Count => _data.Length;
 
-        public T this[int index] => data[index];
+        public INode<T> this[int index] => _data[index];
 
-        public IReadOnlyList<T> FlatView { get; }
+        public IReadOnlyList<INode<T>> FlatView { get; }
         public int FlatCount => FlatView.Count;
         public T Value { get; }
+
+
+        public bool Equals(FlattenableMock<T> other)
+        {
+            return Equals(_data, other._data) && EqualityComparer<T>.Default.Equals(Value, other.Value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is FlattenableMock<T> && Equals((FlattenableMock<T>) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((_data != null ? _data.GetHashCode() : 0) * 397) ^ EqualityComparer<T>.Default.GetHashCode(Value);
+            }
+        }
+
+        public static bool operator ==(FlattenableMock<T> left, FlattenableMock<T> right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(FlattenableMock<T> left, FlattenableMock<T> right)
+        {
+            return !left.Equals(right);
+        }
+
+        public override string ToString()
+        {
+            var inner = _data != null ? string.Join<INode<T>>(", ", _data) : "null";
+            return $"{Value.ToString()} -> {{{inner}}}";
+        }
     }
 }
