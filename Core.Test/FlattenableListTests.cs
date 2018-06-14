b052ac690547498d26f0b478cf7c0bc039536218
Philemon Eichin <phileicht@gmx.de>
2018-06-14 13:23:50 +0200

    Generate TestStub fr FlattenableList

---
+++ Core.Test/FlattenableListTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using phirSOFT.Applications.MusicStand.Core.Test.Mocks;
+
+namespace phirSOFT.Applications.MusicStand.Core.Test
+{
+    [TestFixture()]
+    public class FlattenableListTests
+    {
+        [Test()]
+        public void FlattenableListTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetEnumeratorTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void ClearTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void ContainsTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void CopyToTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void RemoveTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void IndexOfTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void InsertTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void RemoveAtTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
