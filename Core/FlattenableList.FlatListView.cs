fd3c80622f514134b5a57f74ec18809991c89bf4
Philemon Eichin <phileicht@gmx.de>
2018-06-14 12:37:20 +0200

    [WIP] Implement FlattenableList
    Implement Flattenable List, which is used as byte type for SessionSessionSet and SessionSetItemGroup
    
    FlattenableList.FlatListView is only a stub.

---
+++ Core/FlattenableList.FlatListView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace phirSOFT.Applications.MusicStand.Core
+{
+    internal partial class FlattenableList<T>
+    {
+        private class FlattenedListView : IReadOnlyList<T>
+        {
+            public FlattenedListView(FlattenableList<T> parent)
+            {
+                
+            }
+        }
+    }
+}
