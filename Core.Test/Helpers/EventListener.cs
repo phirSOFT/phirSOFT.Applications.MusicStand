48067212f0ef70d30ed50416f392adc4b03511dc
Philemon Eichin <phileicht@gmx.de>
2018-06-16 09:49:22 +0200

    Add EventListener
    Implement an event listener as described in http://mark-dot-net.blogspot.com/2009/09/asserting-events-with-nunit.html

---
+++ Core.Test/Helpers/EventListener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phirSOFT.Applications.MusicStand.Core.Test.Helpers
+{
+    class EventListener<T> where T : EventArgs
+    {
+        private readonly List<T> _savedArgs = new List<T>();
+
+        public EventListener(object raiser, string eventName)
+        {
+            EventInfo eventInfo = raiser.GetType().GetEvent(eventName);
+            var handler = Delegate.CreateDelegate(eventInfo.EventHandlerType, this, "EventHandler");
+            eventInfo.AddEventHandler(raiser, handler);            
+        }
+
+        private void EventHandler(object sender, T args)
+        {
+            _savedArgs.Add(args);
+        }
+
+        public IList<T> SavedArgs => _savedArgs;
+    }
+}
