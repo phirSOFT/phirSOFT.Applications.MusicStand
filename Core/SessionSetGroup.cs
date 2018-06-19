43732357fb12592d971bb04b42298c60aecd79bb
Philemon Eichin <phileicht@gmx.de>
2018-06-19 10:37:59 +0200

    Implement SessionSetTypes
    Add SessionSet and SessionSetGroup. Both are implemented using the FlattenableList class.

---
+++ Core/SessionSetGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using phirSOFT.Applications.MusicStand.Contract;
+
+namespace phirSOFT.Applications.MusicStand.Core
+{
+    internal class SessionSetGroup : FlattenableList<ISessionSetItem>, ISessionSetItemGroup
+    {
+    }
+
+    internal class SessionSet : FlattenableList<ISessionSetItem>, ISessionSet
+    {
+    }
+}
